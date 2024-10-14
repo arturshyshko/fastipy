e907dc679d5e3ec208a3757556fb56daeb684678
arturshyshko <arturshyshko@gmail.com>
2024-10-14 23:23:00 +1100

    Setup basic api/v1/users/...  get endpoints
    Add users/me endpoint for getting current user info.

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,7 +1,9 @@
 from .auth import login_user_via_email_password, logout_user, register_user_via_email_password
 from .health_check import healthcheck
+from .user import UserViewSet
 
 __all__ = [
+    "UserViewSet",
     "healthcheck",
     "login_user_via_email_password",
     "logout_user",
